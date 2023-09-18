59d3f1644093d475aa62cc54105ab8322fae7221
SeongKon <canoa950327@gmail.com>
2023-09-18 18:05:11 +0900

    [추가] User, Expert 테스트케이스

--- expert/tests.py
+++ expert/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.views import status
+
+from .models import *
 
 # Create your tests here.
+class UserCreateTestCase(APITestCase):
+  def setUp(self):
+    self.url = '/v1/expert/create/'
+    self.data = {
+        "username": "sample8@naver.com",
+        "ep_description": "안녕하세요 자기소개입니다",
+        "ep_hours": "매일 오전 11시 ~ 오후 7시",
+        "ep_education": "한국체육대학교 학사",
+        "ep_license": "물리치료사 1급",
+        "ep_career": "국가대표 팀닥터 출신",
+        "ep_product": {
+            "product1": "재활치료 1회권",
+            "product2": "재활치료 5회권",
+            "product3": "재활치료 10회권"
+        },
+        "ep_product_options": {
+            "option1": "운동복",
+            "option2": "락커 이용권"
+        }
+    }
+
+  def test_user_create(self):
+    response = self.client.post(self.url, data=self.data, format='json')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.views import status
+
+from .models import *
 
 # Create your tests here.
+class UserCreateTestCase(APITestCase):
+  def setUp(self):
+    self.url = '/v1/user/create/'
+    self.data = {
+      "username": "sample@sample.com",
+      "password": "1234",
+      "is_superuser": 1,
+      "is_staff": 1,
+      "is_active": 1,
+      "date_joined": "2023-09-18",
+      "mb_phone": "01012345678",
+      "mb_name": "테스트",
+      "mb_nickname": "sample",
+      "mb_certify": True,
+      "mb_services_agree": True,
+      "mb_marketing_agree": False,
+      "mb_privacy_agree": False,
+      "mb_social_type": "d",
+      "mb_level": 1,
+      "mb_birth": "",
+      "mb_gender": ""
+    }
+
+  def test_user_create(self):
+    response = self.client.post(self.url, data=self.data, format='json')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
